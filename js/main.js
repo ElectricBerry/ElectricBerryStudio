3d74f86d92944d8a01f4c19372cb3575a170a939
Electric Berry <vperju074@gmail.com>
2025-10-29 21:01:52 +0200

    Some big changes comming

--- js/main.js
+++ js/main.js
@@ -5,3 +5,11 @@ function scrollToSection(id) {
 function scrollToSection(id) {
     document.getElementById(id).scrollIntoView({ behavior: "smooth" });
 }
+
+const btn = document.getElementById("menuBtn");
+const nav = document.getElementById("navPanel");
+
+btn.addEventListener("click", () => {
+  nav.classList.toggle("show");
+  btn.textContent = nav.classList.contains("show") ? "✕" : "☰";
+});
